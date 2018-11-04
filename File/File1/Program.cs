88848fa6d0653c1991e75da2d1bca20682930af8
Олжас Жаксылыков <40593443+ZhaxylykovOlzhas@users.noreply.github.com>
2018-11-04 23:28:49 +0600

    Add files via upload

---
+++ File/File1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace File1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibonacci = new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946 };
+            string directoryPath = @"/test/docs";
+            string fileName = "file.txt";
+            string driveName = "";
+            string buffer = "", text = "";
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            for (int i = 0; i < drives.Length; i++)
+            {
+                Console.WriteLine($"{i}.{drives[i].Name}");
+            }
+
+            Console.WriteLine("Введите номер диска, на который будет записан файл");
+
+            string driveNumberAsString = Console.ReadLine();
+
+            int driveNumber = 0;
+            if (!int.TryParse(driveNumberAsString, out driveNumber))
+            {
+                Console.WriteLine("Ошибка ввода, будет произведена запись на первый указанный диск.");
+            }
+            driveName = drives[driveNumber].Name;
+
+            if (!Directory.Exists(driveName + directoryPath))
+            {
+                Directory.CreateDirectory(driveName + directoryPath);
+            }
+            try
+            {
+                if (!File.Exists(driveName + directoryPath + fileName))
+                {
+                    File.Create(driveName + directoryPath + fileName);
+                }
+
+
+                using (StreamWriter streamWriter = new StreamWriter(driveName + directoryPath + fileName))
+                {
+                    for (int i = 0; i < 11; i++)
+                    {
+                        text = fibonacci[i].ToString() + ", ";
+                        streamWriter.Write(text);
+                    }
+                }
+
+                Console.WriteLine("Вывод данных из файла:");
+                using (StreamReader streamReader = new StreamReader(driveName + directoryPath + fileName))
+                {
+
+                    buffer = streamReader.ReadToEnd();
+                    Console.WriteLine(buffer);
+                }
+                using (StreamWriter streamWriter = new StreamWriter(driveName + directoryPath + fileName))
+                {
+                    string continueBuffer = "";
+                    for (int i = 11; i < 22; i++)
+                    {
+                        continueBuffer = fibonacci[i].ToString() + ", ";
+                        buffer += continueBuffer;
+                    }
+                    streamWriter.WriteLine(buffer);
+                }
+                using (StreamReader streamReader = new StreamReader(driveName + directoryPath + fileName))
+                {
+                    Console.WriteLine(streamReader.ReadToEnd());
+                }
+            }
+            catch (IOException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ File/File2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string directoryPath = @"/test/second/";
+            string fileInput = "input.txt";
+            string fileOutput = "output.txt";
+            string driveName = "";
+            int firstNumber = 0, secondNumber = 0, sum = 0;
+            bool isPars;
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            for (int i = 0; i < drives.Length; i++)
+            {
+                Console.WriteLine($"{i}.{drives[i].Name}");
+            }
+
+            Console.WriteLine("Введите номер диска, на который будет записан файл");
+
+            string driveNumberAsString = Console.ReadLine();
+
+            int driveNumber = 0;
+            if (!int.TryParse(driveNumberAsString, out driveNumber))
+            {
+                Console.WriteLine("Ошибка ввода, будет произведена запись на первый указанный диск.");
+            }
+            driveName = drives[driveNumber].Name;
+
+            if (!Directory.Exists(driveName + directoryPath))
+            {
+                Directory.CreateDirectory(driveName + directoryPath);
+            }
+            try
+            {
+                if (!File.Exists(driveName + directoryPath + fileInput))
+                {
+                    File.Create(driveName + directoryPath + fileInput);
+                }
+
+
+                using (StreamWriter streamWriter = new StreamWriter(driveName + directoryPath + fileInput))
+                {
+                    Console.WriteLine("\nВведите 1 число:");
+                    isPars = int.TryParse(Console.ReadLine(), out firstNumber);
+
+                    Console.WriteLine("\nВведите 2 число:");
+                    isPars = int.TryParse(Console.ReadLine(), out secondNumber);
+
+                    streamWriter.WriteLine(firstNumber + " " + secondNumber);
+                    sum = firstNumber + secondNumber;
+                    using (StreamWriter streamWriterOutput = new StreamWriter(driveName + directoryPath + fileOutput))
+                    {
+                        streamWriterOutput.WriteLine(sum);
+                    }
+                }
+                using (StreamReader streamReader = new StreamReader(driveName + directoryPath + fileOutput))
+                {
+                    Console.WriteLine("\nСумма:" + streamReader.ReadToEnd());
+                }
+            }
+
+            catch (IOException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            Console.WriteLine("Просмотрите файлы input.txt и output.txt");
+            Console.ReadKey();
+        }
+    }
+}
